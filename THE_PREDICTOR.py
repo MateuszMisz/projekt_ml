5a1b5d3b3600950f0b862349f612065631ce9b7c
MateuszMisz <4002mateo@gmail.com>
2025-01-26 13:01:36 +0100

    final dev version

--- THE_PREDICTOR.py
+++ THE_PREDICTOR.py
@@ -3,9 +3,9 @@ from pathlib import Path
 import pandas as pd
 from argparse import ArgumentParser
 parser=ArgumentParser()
-parser.add_argument("input",help="plik wejsciowy, plik z danymi w formacie csv",required=True)
-parser.add_argument('-s','--show_classifiers',help='wypisuje dostepne klasyfikatory')
-parser.add_argument('-c','--classifiers',type=str,required=True,help='nazwa klasyfikatora ktory bedzie uzyty')
+parser.add_argument('-i',"--input",help="plik wejsciowy, plik z danymi w formacie csv")
+parser.add_argument('-s','--show_classifiers',action="store_true",help='wypisuje dostepne klasyfikatory')
+parser.add_argument('-c','--classifiers',type=str,help='nazwa klasyfikatora ktory bedzie uzyty (wymagane chyba ze uzyto -s)')
 parser.add_argument('-o','--output',type=str,help='plik wyjsciowy, jesli nie podany, wypisuje na wyjscie standardowe')
 args=parser.parse_args()
 MODELS_DIR='./'
@@ -14,12 +14,12 @@ svc=load(Path(MODELS_DIR)/'ostateczne_svc.joblib')
 random_forest=load(Path(MODELS_DIR)/'ostatecz_random_forest.joblib')
 logistic_regression=load(Path(MODELS_DIR)/'ostateczne_logistic_regresion.joblib')
 multinomialNB=load(Path(MODELS_DIR)/'ostateczne_multinomial_naive_bayes.joblib')
-lasso_attributes=['Moda', 'Wolny_czas__spie', 'Sniadanie__nie', 'Zajecie__Czytanie',
+lasso_attributes=['Wolny_czas__spie', 'Sniadanie__nie', 'Zajecie__Czytanie',
        'Paznokcie__nie', 'Wakacje__Praca']
-gini_no_soki_attributes=['Moda', 'Wzrost', 'gry_czas', 'Zajecie__sluchanie muzyki',
+gini_no_soki_attributes=[ 'Wzrost', 'gry_czas', 'Zajecie__sluchanie muzyki',
        'Wakacje__Praca', 'Rod_prz_sam__Przyjaciele', 'Napoj__soki',
        'Napoj__woda']
-entropy_attributes=['Moda', 'Muzyka__Rock', 'Miasto__Poznan', 'Zajecie__Sport',
+entropy_attributes=[ 'Muzyka__Rock', 'Miasto__Poznan', 'Zajecie__Sport',
        'Media_u__TikTok', 'Paznokcie__nie', 'Wakacje__Praca',
        'Wartosc__Kariera', 'Napoj__woda']
 models_and_attributes={
@@ -31,17 +31,19 @@ models_and_attributes={
 }
 
 if args.show_classifiers:
-    print(*models_and_attributes.keys(),sep='\n')
+    print('modele:\twymagane columny:')
+    for key,value in models_and_attributes.items():
+        model_name,required_columns=key,value[1]
+        print(model_name,required_columns,sep='\t')
     exit()
 data=pd.read_csv(args.input)
 choosen_classifiers=args.classifiers
 try:
-    df=data[models_and_attributes[choosen_classifiers][1]
+    df=data[models_and_attributes[choosen_classifiers][1]]
 except:
     print('brakuje kolumny, potrzebne kolumny:',*models_and_attributes[choosen_classifiers][1],sep=' ')
     exit()
-X=df.drop('Moda',axis=1)
-y=df['Moda']
+X=df
 classifier=models_and_attributes[choosen_classifiers][0]
 predict_y=classifier.predict(X)
 print(*predict_y,sep=',')
